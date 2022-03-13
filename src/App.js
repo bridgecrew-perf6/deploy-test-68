5a5f6a39b1087e19b5db7f245a6142347580c178
kasrayounessi <kasyounessi@gmail.com>
2022-03-13 11:15:57 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Rebuilding  the site....</h1>
+      <Navbar />
+
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import './style.css';
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top custom-navbar" id="mainNav">
+      <a class="navbar-brand" href="/">
+        <div class="custom-org-name">Hot Lunch Box</div>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#aboutUs">
+              About Us
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#menu">
+              Menu
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contactUs">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
